6f5b6148ae700e34fdcab11e948125aef96154aa
Aaraav <aaraav2810@gmail.com>
2024-07-07 23:05:18 +0530

    ekdk

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,38 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Home from './components/Home';
 import Redirect from './components/Redirect';
-
+import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
 const Not=()=>{
   return(
     <h1>jcwdwdwcd</h1>
   )
 }
-const App = () => (
-  <Router>
-    <div>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="home" element={<Home />} />
-        <Route path='/x' element={<Redirect/>}/>
+function App() {
+  const { isAuthenticated } = useKindeAuth();
+  
+  if(isAuthenticated){
+  return <Home/> ;
 
-        <Route path='*' element={<Not/>}/>
-        
-      </Routes>
-    </div>
-  </Router>
-);
+  }
+  else {
+    return <Signup/>
+  }
+  // return (
+
+    
+  //   // <Router>
+  //   //   <div>
+  //   //     <Routes>
+  //   //       <Route path="/" element={<Signup />} />
+  //   //       <Route path="home" element={<Home />} />
+  //   //       <Route path='/x' element={<Redirect/>}/>
+
+  //   //       <Route path='*' element={<Not/>}/>
+  //   //     </Routes>
+  //   //   </div>
+  //   // </Router>
+  // )
+
+}
 
 export default App;
