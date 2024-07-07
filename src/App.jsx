e4ab74fe13296cec82133ef68e7d469c65842d63
Aaraav <aaraav2810@gmail.com>
2024-07-07 21:17:24 +0530

    ekdk

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Home from './components/Home';
 
+const Not=()=>{
+  return(
+    <h1>jcwdwdwcd</h1>
+  )
+}
 const App = () => (
   <Router>
     <div>
       <Routes>
-        <Route path="" element={<Signup />} />
+        <Route path="/" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path='*' element={<Not/>}/>
       </Routes>
     </div>
   </Router>
