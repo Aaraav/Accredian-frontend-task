a99e4025196a29cd033b178bb18ab95b58e57de2
Aaraav <aaraav2810@gmail.com>
2024-07-07 19:51:20 +0530

    god

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'eact-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Home from './components/Home';
 import AuthCallback from './components/AuthCallback';
