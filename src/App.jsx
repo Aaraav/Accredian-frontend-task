ed85ec917488794537c5ff08b2174f98d7f53955
Aaraav <aaraav2810@gmail.com>
2024-07-07 23:34:21 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Home from './components/Home';
-import Redirect from './components/Redirect';
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
-const Not=()=>{
-  return(
-    <h1>jcwdwdwcd</h1>
-  )
-}
+
 function App() {
   const { isAuthenticated } = useKindeAuth();
   if(isAuthenticated){
   return <Home/> ;
-
   }
   else {
     return <Signup/>
