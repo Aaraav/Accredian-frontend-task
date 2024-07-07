983f9fcd7a4c2cbb290c64672a8735fd771decea
Aaraav <aaraav2810@gmail.com>
2024-07-07 15:10:39 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {KindeProvider} from "@kinde-oss/kinde-auth-react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
+const clientId = import.meta.env.VITE_KINDE_CLIENT_ID;
+const domain = import.meta.env.VITE_KINDE_DOMAIN;
+const logoutUri = import.meta.env.VITE_LOGOUT_URI;
+const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+
+console.log(clientId);
 ReactDOM.createRoot(document.getElementById('root')).render(
-
   <KindeProvider
-    clientId="5b5597eca74b44d986d309a0a7984111"
-    domain="https://student2810.kinde.com"
-    logoutUri="http://localhost:5173"
-    redirectUri="http://localhost:5173/home"
+    clientId={clientId}
+    domain={domain}
+    logoutUri={logoutUri}
+    redirectUri={redirectUri}
   >
-        <App />
-    </KindeProvider>
-
-)
+    <App />
+  </KindeProvider>
+);
